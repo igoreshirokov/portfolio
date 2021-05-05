91a45a5b092cb6405b7afe357787496b6b28e103
igoreshirokov <igoreshirokov@yandex.ru>
2021-05-06 02:29:26 +0300

    1

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
-import '../styles/globals.css'
-
+import '../styles/globals.sass'
+import '../styles/burgermenu.css'
+import 'swiper/swiper.scss'
+import '../styles/page-slider.sass'
+import 'swiper/components/scrollbar/scrollbar.min.css'
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
